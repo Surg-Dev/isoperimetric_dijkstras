36348827f8bd376acd474030cc4ea1b8c745c194
Sam Ruggerio <samuelr6@illinois.edu>
2023-09-07 23:48:14 -0500

    ported over visualization and such

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 #include "polyscope/polyscope.h"
 #include "polyscope/surface_mesh.h"
+#include "polyscope/pick.h"
 
 #include "args/args.hxx"
 #include "imgui.h"
@@ -31,28 +32,39 @@ polyscope::SurfaceMesh *psMesh = NULL;
 // Some algorithm parameters
 float param1 = 42.0;
 
-// Example computation function -- this one computes and registers a scalar
-// quantity
-void doWork() {
-  polyscope::warning("Computing Gaussian curvature.\nalso, parameter value = " +
-                     std::to_string(param1));
+size_t cut_index = 0;
 
-  nm->geometry->requireVertexGaussianCurvatures();
-  psMesh->addVertexScalarQuantity("curvature",
-                                  nm->geometry->vertexGaussianCurvatures,
-                                  polyscope::DataType::SYMMETRIC);
-}
-
-// A user-defined callback, for creating control panels (etc)
-// Use ImGUI commands to build whatever you want here, see
-// https://github.com/ocornut/imgui/blob/master/imgui.h
 void myCallback() {
 
   if (ImGui::Button("do work")) {
-    doWork();
+    nm->sweepline_dist_process();
   }
 
-  ImGui::SliderFloat("param", &param1, 0., 100.);
+  if (ImGui::Button("Visualize")){
+    std::vector<std::array<double, 3>> fcolors(nm->mesh->nFaces(), {.5,.5,.5});
+    if (cut_index >= nm->neck_ratio_faces_list.size()){
+      cut_index = 0;
+    }
+    auto faceset = nm->neck_ratio_faces_list[cut_index];
+    for (Face f : faceset){
+      fcolors[f.getIndex()] = {1.0, 0.0, 0.0};
+    }
+    psMesh->addFaceColorQuantity("fcolor", fcolors);
+
+    cut_index++;
+  }
+
+  if (ImGui::Button("Set Source")){
+    // Get selection from polyscope
+    auto x = polyscope::pick::getSelection();
+    polyscope::Structure * strc = x.first;
+    auto ind = x.second;
+    if (ind < psMesh->nVertices()){
+      nm->_source = nm->mesh->vertex(ind);
+    }
+  }
+
+  // ImGui::SliderFloat("param", &param1, 0., 100.);
 }
 
 int main(int argc, char **argv) {
@@ -85,52 +97,15 @@ int main(int argc, char **argv) {
   // Set the callback function
   polyscope::state::userCallback = myCallback;
 
-  // Load mesh
-  // std::tie(mesh, geometry) = readManifoldSurfaceMesh(args::get(inputFilename));
   NeckModel nmtemp = NeckModel(args::get(inputFilename));
   nm = std::unique_ptr<NeckModel>(std::move(&nmtemp));
 
-  // Register the mesh with polyscope
-  // psMesh = polyscope::registerSurfaceMesh(
-  //     polyscope::guessNiceNameFromPath(args::get(inputFilename)),
-  //     geometry->inputVertexPositions, mesh->getFaceVertexList(),
-  //     polyscopePermutations(*mesh));
   psMesh = polyscope::registerSurfaceMesh(
       polyscope::guessNiceNameFromPath(args::get(inputFilename)),
       nm->geometry->inputVertexPositions, nm->mesh->getFaceVertexList());
   auto perms = polyscopePermutations(*(nm->mesh));
   psMesh->setAllPermutations(perms);
-  // // Set vertex tangent spaces
-  // nm->geometry->requireVertexTangentBasis();
-  // VertexData<Vector3> vBasisX(*mesh);
-  // for (Vertex v : mesh->vertices()) {
-  //   vBasisX[v] = geometry->vertexTangentBasis[v][0];
-  // }
-  // VertexData<Vector3> vBasisY(*mesh);
-  // for (Vertex v : mesh->vertices()) {
-  //   vBasisY[v] = geometry->vertexTangentBasis[v][1];
-  // }
-  // // psMesh->setVertexTangentBasisX(vBasisX);
 
-  // auto vField =
-  //     geometrycentral::surface::computeSmoothestVertexDirectionField(*geometry);
-  // psMesh->addVertexTangentVectorQuantity("VF", vField, vBasisX, vBasisY);
-
-
-  // Run Dijkstra's
-
-
-
-  // Compute Neck Cut Ratio
-
-  // Get independent cycles
-
-  // Optimize cycle
-
-
-
-
-  // Give control to the polyscope gui
   polyscope::show();
 
   return EXIT_SUCCESS;
--- src/neck_model.cpp
+++ src/neck_model.cpp
@@ -16,6 +16,7 @@ NeckModel::NeckModel(std::string filename){
     _dists = VertexData<float>(*mesh, 0.0);
     _prev = VertexData<Vertex>(*mesh);
     _middle = EdgeData<bool>(*mesh, false);
+    _visited = EdgeData<bool>(*mesh, false);
 }
 
 void NeckModel::compute_shortest_paths(Vertex s){
@@ -49,6 +50,7 @@ void NeckModel::compute_shortest_paths(Vertex s){
 void NeckModel::compute_delete_events(){
     // Initialize data structures
     _middle = EdgeData<bool>(*mesh, false);
+    _visited = EdgeData<bool>(*mesh, false);
     _events = eventQueue();
 
     // Comute 
@@ -82,7 +84,104 @@ void NeckModel::sweepline_dist_process(){
     // Set up dists and events
     compute_shortest_paths(_source);
     compute_delete_events();
-
     // Clear/init any hold variables.
+    clean_sweepline_vars();
+    std::set<Halfedge> wavefront;
 
+    // Queue all halfedges out of source
+    for (Halfedge he : _source.outgoingHalfedges()){
+        wavefront.insert(he);
+        ++wavefront_size;
+    }
+
+    float curr_dist = 0;
+    float prev_dist = 0;
+
+    // Process the wavefront
+    while (wavefront_size > 0){
+      // Get the next event.
+      ePair event = _events.top();
+      float curr_dist = event.first;
+      Edge e = event.second;
+      _events.pop();
+      // Track distance covered from last event.
+      total_dist_covered += (curr_dist - prev_dist) * wavefront_size;
+      prev_dist = curr_dist;
+
+      // If the event is a middle edge, we need to remove both from wavefront.
+      _visited[e] = true;
+      if (_middle[e]){
+        wavefront.erase(e.halfedge());
+        wavefront.erase(e.halfedge().twin());
+        wavefront_size -= 2;
+      } else {
+        // Otherwise handle deletion of single edge.
+        wavefront_size -= 1;
+        // We don't know which halfedge might be in the wavefront.
+        wavefront.erase(e.halfedge());
+        wavefront.erase(e.halfedge().twin());
+
+        Vertex v1 = e.halfedge().vertex();
+        Vertex v2 = e.halfedge().twin().vertex();
+        float d1 = _dists[v1];
+        float d2 = _dists[v2];
+
+        // Orient edge calcs so that d1 < d2
+        if (d1 > d2){
+          std::swap(v1, v2);
+          std::swap(d1, d2);
+        }
+
+        // Add new edges to wavefront
+        for (Halfedge he : v2.outgoingHalfedges()){
+          if (!_visited[he.edge()]){
+            wavefront.insert(he);
+            ++wavefront_size;
+          }
+        }
+
+      }
+
+      // If we've reached the end of the wavefront, break now.
+      if (wavefront_size == 0){
+        break;
+      }
+
+      // Compute neck ratio
+      double remaining_dist = (sum_mesh_dist - total_dist_covered);
+      if (remaining_dist < 0) remaining_dist = 0;
+      double neck_cut_ratio_potential = (double)(total_dist_covered >= remaining_dist ? remaining_dist : total_dist_covered) / (double)(wavefront_size*wavefront_size);
+
+      // Update neck ratio if improved.
+      if (std::max(neck_cut_ratio_potential, neck_ratio) == neck_cut_ratio_potential){
+        qprint("New Neck Ratio: " + std::to_string(neck_cut_ratio_potential));
+        neck_ratio = neck_cut_ratio_potential;
+        neck_ratio_pos = curr_dist;
+
+        std::set<Face> faces;
+        for (auto h : wavefront){
+          faces.insert(h.face());
+          faces.insert(h.twin().face());
+        }
+        neck_ratio_faces_list.push_back(faces);
+
+        // Compute cycle and push back cycle
+        
+        // Push back dist marker
+        neck_ratios_pos_list.push_back(curr_dist);
+      }
+    }
+    // 
+    
+}
+
+void NeckModel::clean_sweepline_vars(){
+    total_dist_covered = 0;
+    wavefront_size = 0;
+    neck_ratio = 0;
+    neck_ratio_pos = 0;
+    neck_ratios_pos_list.clear();
+    neck_ratio_vertices_list.clear();
+    neck_ratio_cycle_list.clear();
+    neck_ratio_faces_list.clear(); 
 }
